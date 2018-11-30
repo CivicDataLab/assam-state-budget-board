b8a47ba58e4855e25f3a584e8de2c52bb0d50699
Akshay Verma <akshayverma295@gmail.com>
2018-11-30 15:09:48 +0530

    Initial Commit : Basic Setup

---
+++ assam_budget_board/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for assam_budget_board project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assam_budget_board.settings")
+
+application = get_wsgi_application()
