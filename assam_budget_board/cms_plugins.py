fad99fccde5a5883ca0e4ba9ef8aa933a2bae889
ubuntu <shreyaagrawal0809@gmail.com>
2019-01-14 10:27:03 +0530

    Added receipts data representation as a single page

--- assam_budget_board/cms_plugins.py
+++ assam_budget_board/cms_plugins.py
@@ -2,7 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
-from .models import ExpenditureGrant
+from .models import ExpenditureGrant, Receipts
 
 @plugin_pool.register_plugin
 class ExpenditureGrantPlugin(CMSPluginBase):
@@ -21,6 +21,31 @@ class ExpenditureGrantPlugin(CMSPluginBase):
         		"actuals" : "2016-17 Actuals",
 				"bePrev" : "2017-18 Budget Estimates",
 				"re" : "2017-18 Revised Estimates"
+        		},
+        		'headHierarchy' : headHierarchy,
+        		'tableColumns' : tableColumns,
+        	})
+
+        return context
+
+
+@plugin_pool.register_plugin
+class ReceiptsPlugin(CMSPluginBase):
+    model = Receipts
+    name = _("Receipts")
+    render_template = "receipts.html"
+    cache = True
+
+    def render(self, context, instance, placeholder):
+        context = super(ReceiptsPlugin, self).render(context, instance, placeholder)
+        headHierarchy = "GRANT NUMBER, Major Head, Sub-Major Head, Minor Head, Sub-Minor Head, Detailed Head, Object Head, Voucher Head"
+        tableColumns = "HEAD OF ACCOUNT,HEAD DESCRIPTION"
+        context.update({
+        	"fiscalYear" : {
+        		"be" : "BUDGET 2018-19", 
+        		"actuals" : "ACTUALS 2016-17",
+				"bePrev" : "BUDGET 2017-18",
+				"re" : "REVISED 2017-18"
         		},
         		'headHierarchy' : headHierarchy,
         		'tableColumns' : tableColumns,
--- assam_budget_board/settings.py
+++ assam_budget_board/settings.py
@@ -178,6 +178,7 @@ CMS_TEMPLATES = (
     ('sidebar_right.html', 'Sidebar Right'),
     ('home.html', 'Homepage'),
     ('content_1.html', 'Content Structure 1'),
+    ('content_2.html', 'content Structure 2'),
     ('grants.html', 'All Grants'),
     ('page.html', 'Page')
 )
