916d52b09f0a7631f1a57f905586d4f5c0bf24b2
ubuntu <shreyaagrawal0809@gmail.com>
2019-01-21 18:34:53 +0530

    Added generic plugin for datatable

--- assam_budget_board/cms_plugins.py
+++ assam_budget_board/cms_plugins.py
@@ -2,7 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
-from .models import ExpenditureGrant, Receipts, AllGrants
+from .models import ExpenditureGrant, Receipts, AllGrants, Datatable
 
 
 
@@ -74,5 +74,15 @@ class SmallMultiplesExpPlugin(CMSPluginBase):
         		},
         		'tableColumns' : tableColumns
         	})
+        return context
+@plugin_pool.register_plugin
+class DataTablePlugin(CMSPluginBase):
+    model = Datatable
+    name = _("Datatable")
+    render_template = "datatable.html"
+    cache = True
+
+    def render(self, context, instance, placeholder):
+        context = super(DataTablePlugin, self).render(context, instance, placeholder)
         return context
 
