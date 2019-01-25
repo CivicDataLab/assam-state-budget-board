412e037d1a674dd9bab0b350fb49ba2449577b92
ubuntu <shreyaagrawal0809@gmail.com>
2019-01-25 15:13:46 +0530

    Added for loop for page generation

--- assam_budget_board/scripts/pages.py
+++ assam_budget_board/scripts/pages.py
@@ -4,9 +4,15 @@ from cms.models.placeholdermodel import Placeholder
 from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-
-def run():
-    page = api.create_page(title="Sub-grant",language='en',template='content_1.html',parent= Page.objects.get(pk=1))
-    placeholder = page.placeholders.get(slot='grant placeholder')
+urls = open('/home/ubuntu/Downloads/assam_csv_urls_2018.txt', "r")
+url = []
+for x in urls:
+    url.append(x.strip('\n'))
     
-    api.add_plugin(placeholder,'ExpenditureGrantPlugin', language = 'en', position = 'first-sibling', **{'url': 'https://openbudgetsindia.org/dataset/69b68cf0-d032-4cf2-a842-72f9936c7009/resource/932d3e42-cfaf-4602-8fdc-91d25bd2fd64/download/24---aid-materials.csv'})
+    
+def run():
+    for i in range(0,81):
+        page = api.create_page(title="Sub-grant",language='en',template='content_1.html',parent= Page.objects.get(pk=1))
+        placeholder = page.placeholders.get(slot='grant placeholder')
+    
+        api.add_plugin(placeholder,'ExpenditureGrantPlugin', language = 'en', position = 'first-sibling', **{'url': url[i]})
