09ddaca6624a0c3c45796ffc808b292b9e58edab
ubuntu <shreyaagrawal0809@gmail.com>
2019-01-25 15:42:31 +0530

    Added array for different titles

--- assam_budget_board/scripts/read_urls.py
+++ assam_budget_board/scripts/read_urls.py
@@ -1,7 +1,6 @@
-urls = open('/home/ubuntu/Downloads/assam_csv_urls_2018.txt', "r")
-url = []
-def run():
-    for x in urls:
-        url.append(x.strip('\n'))
-    print(url)
+from cms import api
+from cms.models import Page
+pages = Page.objects.all()
+print(pages)
+
     
