daca740a504531e3df58111f93f68b0a1689d112
ubuntu <shreyaagrawal0809@gmail.com>
2019-02-07 13:43:43 +0530

    Script

--- assam_budget_board/scripts/pages.py
+++ assam_budget_board/scripts/pages.py
@@ -5,21 +5,21 @@ from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
-
-urls = open('/home/ubuntu/Downloads/obi_links.txt', "r")
-title = open('/home/ubuntu/Downloads/page_titles.txt', "r")
+title = open('assam_budget_board/static/page_titles', "r")
+urls = open('assam_budget_board/static/urls.txt', "r")
 url = []
 titles = []
 for x in urls:
    url.append(x.strip('\n'))
+
 for y in title:
    titles.append(y.strip('\n'))
   
 def run():
     for i in range(0,81):
-       page = api.create_page(title= titles[i],language='en',template='content_1.html',parent= Page.objects.get(pk=324))
+       page = api.create_page(title= titles[i],language='en',template='content_1.html',parent= Page.objects.get(pk=12))
        placeholder = page.placeholders.get(slot='grant placeholder')
-  
        api.add_plugin(placeholder,'ExpenditureGrantPlugin', language = 'en', position = 'first-sibling', **{'url': url[i]})
+  
 
 
